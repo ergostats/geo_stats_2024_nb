916938e8589d1613dee54557245e067452dce1de
Alex Bajaña <alexb.inf.4891@gmail.com>
2024-05-31 16:32:35 -0500

    Cambios para Pablo

--- script.R
+++ script.R
@@ -1,22 +1,17 @@
-<<<<<<< HEAD
 
-archivo <- "2009586.zip"
-
- unzip(archivo,list = TRUE)
- 
- unzip(archivo)
-
-
-
-=======
 # Como realizar un Unzip archivos 
 
-archivo <- "C:/Users/alex_ergostats/Documents/geo_stats_2024_nb/2009586.zip"
+archivo <- "2009586.zip"
 
 # Se visualiza los documentos que se pueden unzip
  unzip(archivo,list = TRUE)
  
 # Aquí se realiza el unzip
  unzip(archivo)
->>>>>>> 6c15d1e40df9720764a2aa31ad44521aa424ff76
+
+ # Abro el otro archivo comprimido que me interesa
+ 
+ unzip("NEON-DS-Airborne-Remote-Sensing.zip")
+ 
+ 
  
