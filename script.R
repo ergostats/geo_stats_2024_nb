6c15d1e40df9720764a2aa31ad44521aa424ff76
Pablo_S <pablofsh@ergostats.org>
2024-05-31 16:23:09 -0500

    Agregar comentarios en el scritp*
    Se agregaron comentarios para entender mejor lo que hace cada comando.

--- script.R
+++ script.R
@@ -1,6 +1,10 @@
+# Como realizar un Unzip archivos 
+
 archivo <- "C:/Users/alex_ergostats/Documents/geo_stats_2024_nb/2009586.zip"
 
+# Se visualiza los documentos que se pueden unzip
  unzip(archivo,list = TRUE)
  
+# Aquí se realiza el unzip
  unzip(archivo)
  
